138d531364a2216867808b4990eeca80eb180398
luiz <luiz_fleite@outlook.com>
2023-09-26 23:43:40 -0300

    Novos testes da classe human com tratamento de string

--- main.cpp
+++ main.cpp
@@ -5,7 +5,6 @@
 #include "human.cpp"
 
 using std::cout;
-using std::endl;
 
 int main(void) {
 
@@ -14,26 +13,42 @@ int main(void) {
     h1.talk();
     h1.walk();
 
-    cout << endl;
+    cout << "\n";
 
     Human h2("Claudomiro", 35, 12125.12, 200, 200);
     h2.print_info();
     h2.talk();
     h2.walk();
 
-    cout << endl;
+    cout << "\n";
     
     Human h3("Geralt");
     h3.print_info();
     h3.talk();
     h3.walk();
 
-    cout << endl;
+    cout << "\n";
 
-    Human h4("", -35, -12125.12, -100, -100);
+    Human h4("   ", -35, -12125.12, -100, -100);
     h4.print_info();
     h4.talk();
     h4.walk();
+
+    cout << "\n";
+
+    Human h5("Shortened Too long name", -1, -1, -1, -1);
+    h5.print_info();
+    h5.talk();
+    h5.walk();
+
+    cout << "\n";
+
+    Human h6("    Name with spaces     ", -1, -1, -1, -1);
+    h6.print_info();
+    h6.talk();
+    h6.walk();
+
+    cout << "\n";
 
     return 0;
 }
