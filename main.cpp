edcd8efd3814a11b0172bf3acdb7215a6092e350
luiz-fleite <102365054+luiz-fleite@users.noreply.github.com>
2023-10-15 23:18:16 -0300

    New tests for Witcher class, Sword class, Battle class, and Witcher's Sword pointer array with resize function

--- main.cpp
+++ main.cpp
@@ -2,59 +2,69 @@
 #include <string>
 #include <unistd.h>  
 
-#include "Entity.h"
 #include "Entity.cpp"
-#include "Human.h"
 #include "Human.cpp"
-#include "Ghoul.h"
 #include "Ghoul.cpp"
-#include "Battle.h"
 #include "Battle.cpp"
+#include "Witcher.cpp"
+#include "Sword.cpp"
 
 using std::cout;
 
 int main(void) {
 
-    cout << "====First Battle:====\n";
-    // durante a batalha o primeiro aldeão é atacado pelo
-    // primeiro ghoul ate q ele morra
+    cout << "====Before first battle====\n";
+    Witcher w1;
+    w1.print_info();
+    Sword s1;
+    w1.addSword(s1);
+    Sword s2("Silver Sword", 20);
+    w1.addSword(s2);
+    w1.print_swords();
+
+    cout << "\n";
+
+    Witcher w2(w1);
+    w2.setName("Geralt");
+
+    cout << "\n";
 
     Human h1;
-    // h1.print_info();
+    h1.print_info();
 
     cout << "\n";
 
     Ghoul g1;
-    // g1.print_info();
-
-    cout << "\n";
-/*
-    Human h2 = Human(h1);
-    h2.print_info();
-
-    cout << "\n";
-
-    Ghoul g2 = Ghoul(g1);
-    g2.print_info();
-
-    cout << "\n";
-
-    h1.attack(g1);
-    g1.attack(h1);
-    g2.attack(g1);
-    h1.print_info();
-    h2.print_info();
     g1.print_info();
-    g2.print_info();
-*/
 
-    Battle b1(h1, g1);
+    cout << "\n";
+    cout << "====First Battle:====\n";
+    // durante a batalha o primeiro aldeão é atacado pelo
+    // primeiro ghoul e o witcher tenta protege-lo
+
+    cout << "\n";
+
+    Battle b1(w1, g1);
     b1.print_allies();
     b1.print_enemies();
     b1.beginBattle();
 
     cout << "\n";
     cout << "====After first battle:====\n";
+    // após a primeira batalha o witcher matou o ghoul e 
+    // o aldeão sobreviveu. O witcher ganha uma nova espada e 
+    // ocorre resize em seu vetor de espadas. A copia do witcher
+    // fica intacta. 
+    cout << "\n";
+
+    Sword s3("Steel Sword", 15);
+    w1.addSword(s3);
+    w1.print_info();
+    w1.print_swords();
+
+    cout << "\n";
+
+    g1.print_info();
 
     cout << "\n";
 
@@ -62,7 +72,8 @@ int main(void) {
 
     cout << "\n";
 
-    g1.print_info();
+    w2.print_info();
+    w2.print_swords();
 
     return 0;
 }
