7f0c7592f8ab223669956d47e9d519bf3d876fce
luiz-fleite <102365054+luiz-fleite@users.noreply.github.com>
2023-10-30 02:49:38 -0300

    New main tests

--- main.cpp
+++ main.cpp
@@ -21,8 +21,14 @@ using std::ifstream;
 using std::istringstream;
 
 int main(void) {
-    Witcher w1 = Witcher();
-    cout << w1 << "\n";
+    // O jogador começa em sua casa, em Kaer Morhen
+    // E tem acesso ao seu baú pessoal de espadas
+    Witcher w1 = Witcher("Geralt");
+    cout << w1;
+    Sword * s2 = new Sword("Silver Sword", 10);
+    w1.add_sword(*s2);
+    w1.equip_sword(0);
+    w1.print_equipped_items();
 
     ifstream inputFile("./Items/swords_chest.txt");
     if (!inputFile.is_open()) {
@@ -50,6 +56,49 @@ int main(void) {
     inputFile.close();
 
     w1.print_chest_swords();
+
+    // Ele sai de sua casa e entra em uma caverna
+    // misteriosa onde tem um ghoul
+    vector <Sword*> cave_floor_swords;
+    Ghoul *g1 = new Ghoul();
+    cout << *g1 << "\n";
+    Sword * s1 = new Sword("Ancient Sword", 20);
+    g1->add_sword(*s1);
+    g1->print_inventory();
+
+    // O jogador entra em batalha com o ghoul
+    w1.attack(*g1);
+    //sleep(1);
+    g1->attack(w1);
+    //sleep(1);
+
+    // se o ghoul morre ele dropa a espada no chão
+    // e é deletado para liberar memória
+    cout << "dropa espada\n";
+    g1->drop_sword(0, cave_floor_swords);
+    cout << "deleta ghoul\n";
+    delete g1;
+    cout << "ghoul deletado\n";
+
+    // o jogador pega a espada do chão
+    cout << "endereço da espada: " << cave_floor_swords[0] << "\n";
+    cout << "espada: " << *cave_floor_swords[0] << "\n";
+    cout << "pega espada\n";
+    w1.add_sword(*cave_floor_swords[0]);
+    cout << "espada pega\n";
+    cout << w1 << "\n";
+    w1.print_inventory();
+
+    // Testes de operadores
+    Witcher w2 = Witcher("Vesemir");
+    cout << "Testes de operadores\n";
+    cout << "Witcher1 == Witcher1: " << (w1 == w1) << "\n";
+    cout << "Witcher1 != Witcher1: " << (w1 != w1) << "\n";
+    cout << "Witcher1 == Witcher2: " << (w1 == w2) << "\n";
+    cout << "Witcher1 != Witcher2: " << (w1 != w2) << "\n";
+    cout << "Witcher1 = Witcher2\n";
+    w1 = w2;
+    cout << "Witcher1 == Witcher2: " << (w1 == w2) << "\n";
 
     return 0;
 }
