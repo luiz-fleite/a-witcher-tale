82b2a5dd26138a0f685b96f3157cedc6c0f1f3ef
luiz-fleite <luiz_fleite@outlook.com>
2023-11-08 17:22:29 -0300

    Working load and save witcher atributes

--- Utils/load_save.h
+++ Utils/load_save.h
@@ -122,7 +122,6 @@ bool save_witcher( Witcher &witcher, map<string, string> &atributes, string name
     output_file << "Witcher = 1\n";
     // Then writes all the atributes according to their type
     for (const auto& pair : atributes) {
-        cout << pair.first << " = " << pair.second << '\n';
         output_file << pair.first << " = " << pair.second << '\n';
     }
     output_file.close();
@@ -131,42 +130,6 @@ bool save_witcher( Witcher &witcher, map<string, string> &atributes, string name
 }
 
 /*
-bool loadConfig( map<string, double>& variables, string nameFile )
-{
-
-    ifstream inputFile("config_witcher.txt");
-
-    if ( !inputFile.is_open() ) 
-    {
-        cerr << "Error opening file." << '\n';
-        return false; // Exit with an error code
-    }
-
-    string line;    
-    string variableName;
-    char equalsSign;
-    double value;
-    while (getline(inputFile, line)) 
-    {
-        istringstream iss(line);
-        if (iss >> variableName >> equalsSign >> value && equalsSign == '=') 
-            variables[variableName] = value;
-        else
-            cerr << "Error parsing line: " << line << '\n';
-    }
-
-    if( variables.size( ) == 0 )
-    {   
-        cerr << "No variables found.\n";
-        return false;
-    }
-
-    inputFile.close();
-
-    return true;
-
-}
-
 bool loadConfig( map<string, string>& stringVariables, map<string, double>& doubleVariables, string nameFile )
 {
 
@@ -215,63 +178,8 @@ bool loadConfig( map<string, string>& stringVariables, map<string, double>& doub
     return true;
 
 }
-
-
-bool saveConfigs( map<string, double>& variables, string nameFile )
-{
-    // abre o arquivo pra escrita 
-    ofstream outputFile(nameFile, std::ios::out | std::ios::trunc);
-    if (!outputFile.is_open()) {
-        cerr << "Erro ao abrir aquivo para escrita!" << '\n';
-        return false; //
-    }
-
-    for (const auto& pair : variables)
-        outputFile << pair.first << " = " << pair.second << '\n';
-    
-    // escreve no arquivo 
-    /*  
-    outputFile << "dia = " << dia << '\n';
-    outputFile << "mes = " << mes << '\n';
-    outputFile << "ano = " << ano << '\n';
-    */
-    /*
-    outputFile.close();
-    cout << "Dados salvos com sucesso!" << '\n';
-    return true;
-}
-
-
-void createInfos( string nameFile, int id = 0 )
-{
-    int dia;
-    int mes;
-    int ano;
-
-    cout << "Para jogar, insira sua data de nascimento (dia mês ano): ";
-    if (!(cin >> dia >> mes >> ano)) {
-        cerr << "entrada inválida!" << '\n';
-        return; // Exit with an error code
-    }
-
-    // abre o arquivo pra escrita 
-    ofstream outputFile(nameFile, std::ios::out | std::ios::trunc);
-    if (!outputFile.is_open()) {
-        cerr << "Erro ao abrir aquivo para escrita!" << '\n';
-        return; //
-    }
-		// escreve no arquivo 
-    outputFile << "id = " << id << '\n';
-    outputFile << "dia = " << dia << '\n';
-    outputFile << "mes = " << mes << '\n';
-    outputFile << "ano = " << ano << '\n';
-
-    outputFile.close();
-    cout << "Dados salvos com sucesso!" << '\n';
-    return;
-}
-
 */
+
 
 /*if (std::getline(iss, value)) {: This line is used to read the rest of the line 
 from the iss stream and store it in the value variable. It reads characters from 
